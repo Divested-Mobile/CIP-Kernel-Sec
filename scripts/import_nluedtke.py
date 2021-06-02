5865cf39bc18e6e5cd92aa748b6bc04327bc8d63
Pavel Machek <pavel@ucw.cz>
2021-06-02 13:34:44 +0200

    Print list of vulnerabilities not in our database.
    Signed-off-by: Pavel Machek <pavel@denx.de>

--- scripts/import_nluedtke.py
+++ scripts/import_nluedtke.py
@@ -3,16 +3,30 @@
 import os
 
 class IssueList:
-    pass
+    issues = "./issues/"
+    
+    def issue(m, cve):
+        if os.path.exists(m.issues + cve + ".yml"):
+            return True
+        return None
 
 class KernelCVE(IssueList):
     kernel_cves = "../linux_kernel_cves"
 
     def check(m, version):
         path = m.kernel_cves + "/data/" + version + "/" + version + "_CVEs.txt"
+        total = 0
+        known = 0
         for l in open(path).readlines():
             s = l.split(":")
-            print(s[0], s[1])
+            cve = s[0]
+            total += 1
+            if m.issue(cve):
+                known += 1
+            else:
+                print(l)
+        print(total-known, "/", total)
+
         
 i = KernelCVE()
 i.check("5.10")
