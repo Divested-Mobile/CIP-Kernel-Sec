7647fed7c1535cda3d460448b63d424f50ca3269
Masami Ichikawa <masami.ichikawa@miraclelinux.com>
2024-04-19 11:10:27 +0900

    scripts/report_affected: add feature to show ignored/fixed status
    This commit added following features
    - Show ignored and fixed status
    - Output yaml format dump and save to file
    
    To keep old behavior, above features are enabled when yaml format is
    specified to --output-format option.
    
    Signed-off-by: Masami Ichikawa <masami.ichikawa@miraclelinux.com>

--- scripts/kernel_sec/issue.py
+++ scripts/kernel_sec/issue.py
@@ -332,7 +332,11 @@ def get_id_sort_key(cve_id):
     return _cve_id_arbdig_re.sub(lambda m: '-%07d' % int(m.group(1)), cve_id)
 
 
-def affects_branch(issue, branch, is_commit_in_branch):
+ISSUE_STATUS_NOT_AFFECTED = 'not affected'
+ISSUE_STATUS_NOT_FIXED = 'not fixed'
+ISSUE_STATUS_FIXED = 'fixed'
+
+def status_on_branch(issue, branch, is_commit_in_branch):
     branch_name = branch['short_name']
 
     # If it was not introduced on this branch, and was introduced on
@@ -341,25 +345,30 @@ def affects_branch(issue, branch, is_commit_in_branch):
     if introduced:
         if introduced.get('mainline') == 'never' and \
            (branch_name == 'mainline' or branch_name not in introduced):
-            return False
+            return ISSUE_STATUS_NOT_AFFECTED
         if branch_name not in introduced:
             for commit in introduced['mainline']:
                 if is_commit_in_branch(commit, branch):
                     break
             else:
-                return False
+                return ISSUE_STATUS_NOT_AFFECTED
 
     # If it was fixed on this branch, or fixed on mainline before
     # the branch point, branch is not affected
     fixed = issue.get('fixed-by', {})
     if fixed:
         if fixed.get(branch_name, 'never') != 'never':
-            return False
+            return ISSUE_STATUS_NOT_AFFECTED
         if fixed.get('mainline', 'never') != 'never':
             for commit in fixed['mainline']:
                 if not is_commit_in_branch(commit, branch):
                     break
             else:
-                return False
+                return ISSUE_STATUS_FIXED
 
-    return True
+    return ISSUE_STATUS_NOT_FIXED
+
+def affects_branch(issue, branch, is_commit_in_branch):
+    status = status_on_branch(issue, branch, is_commit_in_branch)
+
+    return status == ISSUE_STATUS_NOT_FIXED
